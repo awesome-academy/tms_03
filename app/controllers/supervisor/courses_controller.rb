06b4bd7b7460515fe484f0e8588160f7ec1d2126
nguyenthanhtu95 <nguyen.thanh.tu@sun-asterisk.com>
2019-06-12 16:34:24 +0700

    CRUD courses & trainee users

---
+++ app/controllers/supervisor/courses_controller.rb
@@ -0,0 +1,59 @@
+class Supervisor::CoursesController < SupervisorController
+  before_action :load_course, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @courses = Course.sorted
+                     .paginate(page: params[:page], per_page: Settings.per_page)
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    course = Course.new(course_params)
+    course.created_by = current_user.id
+    if course.save
+      flash[:success] = t(:created, resource: Course.name)
+      redirect_to supervisor_courses_path
+    else
+      false[:danger] = t(:wrong_msg)
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    if @course.update(course_params)
+      flash[:success] = t(:course_updated)
+      redirect_to supervisor_courses_path
+    else
+      false[:danger] = t(:wrong_msg)
+      render :edit
+    end
+  end
+
+  def destroy
+    if @course.destroy
+      flash[:success] = t(:deleted, resource: Course.name)
+    else
+      false[:danger] = t(:wrong_msg)
+    end
+    redirect_to supervisor_courses_path
+  end
+
+  private
+
+  def load_course
+    @course = Course.find_by(id: params[:id])
+    return if @course
+    render plain: t(:not_found)
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+end
--- app/controllers/supervisor/users_controller.rb
+++ app/controllers/supervisor/users_controller.rb
@@ -1,3 +1,46 @@
 class Supervisor::UsersController < SupervisorController
-  def index; end
+  before_action :load_user, only: :destroy
+
+  def index
+    @users = User.sorted
+                 .paginate(page: params[:page], per_page: Settings.per_page)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.role = User.roles[:trainee]
+    @user.password = Settings.password
+    @user.password_confirmation = Settings.password
+    if @user.save
+      flash[:success] = t(:done)
+      redirect_to supervisor_users_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t("delete", resource: User.name)
+    else
+      flash[:danger] = t(:wrong_msg)
+    end
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    render plain: t(:user_not_found)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
 end
